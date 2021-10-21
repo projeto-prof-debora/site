ac5f61ab879323677365f83d5341d681aec4bd93
bot-adriano <89438737+bot-adriano@users.noreply.github.com>
2021-10-21 04:10:22 -0300

    3

--- cadastro.php
+++ cadastro.php
@@ -1,9 +1,7 @@
 <?php
-$vemail=$_POST["f_email"];
-    $vnome=$_POST["f_nome"];
-    $vsenha=$_POST["f_senha"];
-    $vcpf=$_POST["f_cpf"];
-    $vcep=$_POST["f_cep"];
-    echo "email: ".$vemail."<br/>nome: ".$vnome."<br/>senha: ".$vsenha."<br/cpf: >".$vcpf."<br/>cep: ".$vcep."<br/>";
-
- ?>
+include "conexao.inc";/*chama a conexao.incque é o atalho pra chamar o db*/
+$res=mysqli_query($con,"SELECT * from tb_cadastro");/*informamos o nome da conexao"$con"criamos uma variavel "res" para poder usar o comando my sql*/
+$linhas=mysqli_num_rows($res);/*comando pra retornar o numero de linhas esta retornando*/
+echo "encontrados $linhas registros na tbela tb_cadastros";/*mostra a qtd de registro na tabela*/
+mysqli_close($con);/*comando para fechar a tabela*/
+?>
